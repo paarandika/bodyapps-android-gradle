aed099f8b5155d11d37308dcc6fa3fe6c94631ff
Ayantha Randika <paarandika@users.noreply.github.com>
2014-08-14 00:39:04 +0530

    Update SyncAdapterTest.java

--- app/src/androidTest/java/org/fashiontec/bodyapps/sync/test/SyncAdapterTest.java
+++ app/src/androidTest/java/org/fashiontec/bodyapps/sync/test/SyncAdapterTest.java
@@ -20,30 +20,30 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class SyncAdapterTest extends AndroidTestCase {
-    // Context context;
-    // public static final String AUTHORITY = "org.fashiontec.bodyapps.sync.provider";
-    // public static final String ACCOUNT_TYPE = "fashiontec.org";
-    // public static final String ACCOUNT = "dummyaccount";
+    Context context;
+    public static final String AUTHORITY = "org.fashiontec.bodyapps.sync.provider";
+    public static final String ACCOUNT_TYPE = "fashiontec.org";
+    public static final String ACCOUNT = "dummyaccount";
 
-    // @Override
-    // public void setUp() throws Exception {
-    //     context = getContext().getApplicationContext();
-    //     SyncUser su = new SyncUser();
-    //     String userID = su.getUserID("rand@email2.com", "rand");
-    //     Measurement measurement = new Measurement("eb35a165-8e22-4cee-a9b0-7196e3", userID, 1, 1);
-    //     MeasurementManager.getInstance(context).addMeasurement(measurement);
-    //     MeasurementManager.getInstance(context).setSyncedOnce("eb35a165-8e22-4cee-a9b0-7196e3");
-    // }
+    @Override
+    public void setUp() throws Exception {
+        context = getContext().getApplicationContext();
+        SyncUser su = new SyncUser();
+        String userID = su.getUserID("rand@email2.com", "rand");
+        Measurement measurement = new Measurement("eb35a165-8e22-4cee-a9b0-7196e3", userID, 1, 1);
+        MeasurementManager.getInstance(context).addMeasurement(measurement);
+        MeasurementManager.getInstance(context).setSyncedOnce("eb35a165-8e22-4cee-a9b0-7196e3");
+    }
 
-    // public void testSync() throws InterruptedException {
-    //     Account newAccount = new Account(ACCOUNT, ACCOUNT_TYPE);
-    //     AccountManager accountManager = (AccountManager) context.getSystemService(context.ACCOUNT_SERVICE);
-    //     accountManager.addAccountExplicitly(newAccount, null, null);
-    //     ContentResolver.setSyncAutomatically(newAccount, AUTHORITY, true);
-    //     ContentResolver.requestSync(newAccount, "org.fashiontec.bodyapps.sync.provider", Bundle.EMPTY);
-    //     final CountDownLatch signal = new CountDownLatch(1);
-    //     signal.await(30, TimeUnit.SECONDS);
-    //     assertNull("Send measurement not happened", MeasurementManager.getInstance(context).getMeasurementSync());
-    // }
+    public void testSync() throws InterruptedException {
+        Account newAccount = new Account(ACCOUNT, ACCOUNT_TYPE);
+        AccountManager accountManager = (AccountManager) context.getSystemService(context.ACCOUNT_SERVICE);
+        accountManager.addAccountExplicitly(newAccount, null, null);
+        ContentResolver.setSyncAutomatically(newAccount, AUTHORITY, true);
+        ContentResolver.requestSync(newAccount, "org.fashiontec.bodyapps.sync.provider", Bundle.EMPTY);
+        final CountDownLatch signal = new CountDownLatch(1);
+        signal.await(30, TimeUnit.SECONDS);
+        assertNull("Send measurement not happened", MeasurementManager.getInstance(context).getMeasurementSync());
+    }
 
 }
