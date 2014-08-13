87c760d85d59de430afce9311b7c8fd424fde45a
Ayantha Randika <paarandika@users.noreply.github.com>
2014-08-13 15:53:48 +0530

    Update MainActivityTest.java

--- app/src/androidTest/java/org/fashiontec/bodyapps/main/test/MainActivityTest.java
+++ app/src/androidTest/java/org/fashiontec/bodyapps/main/test/MainActivityTest.java
@@ -53,33 +53,33 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         assertTrue(val);
     }
 
-    public void testSavedActivity() throws Exception {
-        Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(SavedActivity.class.getName(), null, false);
-        TouchUtils.clickView(this, saved);
-        SavedActivity startedActivity = (SavedActivity) monitor.waitForActivityWithTimeout(1000);
-        assertNotNull(startedActivity);
-        this.sendKeys(KeyEvent.KEYCODE_BACK);
-    }
+    // public void testSavedActivity() throws Exception {
+    //     Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(SavedActivity.class.getName(), null, false);
+    //     TouchUtils.clickView(this, saved);
+    //     SavedActivity startedActivity = (SavedActivity) monitor.waitForActivityWithTimeout(1000);
+    //     assertNotNull(startedActivity);
+    //     this.sendKeys(KeyEvent.KEYCODE_BACK);
+    // }
 
-    public void testSettingsActivity() throws Exception {
-        Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(SettingsActivity.class.getName(), null, false);
-        TouchUtils.clickView(this, settings);
-        SettingsActivity startedActivity = (SettingsActivity) monitor.waitForActivityWithTimeout(1000);
-        assertNotNull(startedActivity);
-        this.sendKeys(KeyEvent.KEYCODE_BACK);
-    }
+    // public void testSettingsActivity() throws Exception {
+    //     Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(SettingsActivity.class.getName(), null, false);
+    //     TouchUtils.clickView(this, settings);
+    //     SettingsActivity startedActivity = (SettingsActivity) monitor.waitForActivityWithTimeout(1000);
+    //     assertNotNull(startedActivity);
+    //     this.sendKeys(KeyEvent.KEYCODE_BACK);
+    // }
 
-    public void testCreateActivity() throws Exception {
-        Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(CreateActivity.class.getName(), null, false);
-        TouchUtils.clickView(this, create);
-        CreateActivity startedActivity = (CreateActivity) monitor.waitForActivityWithTimeout(1000);
-        EditText email = (EditText) startedActivity.findViewById(R.id.create_txt_gmail);
-        InputMethodManager imm = (InputMethodManager)activity.getBaseContext().getSystemService(
-                activity.getBaseContext().INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(email.getWindowToken(), 0);
-        assertNotNull(startedActivity);
-        this.sendKeys(KeyEvent.KEYCODE_BACK);
-    }
+    // public void testCreateActivity() throws Exception {
+    //     Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(CreateActivity.class.getName(), null, false);
+    //     TouchUtils.clickView(this, create);
+    //     CreateActivity startedActivity = (CreateActivity) monitor.waitForActivityWithTimeout(1000);
+    //     EditText email = (EditText) startedActivity.findViewById(R.id.create_txt_gmail);
+    //     InputMethodManager imm = (InputMethodManager)activity.getBaseContext().getSystemService(
+    //             activity.getBaseContext().INPUT_METHOD_SERVICE);
+    //     imm.hideSoftInputFromWindow(email.getWindowToken(), 0);
+    //     assertNotNull(startedActivity);
+    //     this.sendKeys(KeyEvent.KEYCODE_BACK);
+    // }
     
 
 }
